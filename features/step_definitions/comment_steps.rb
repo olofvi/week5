963cd1ad663d7a5541a7c6b9614ebb823dccaa3d
olofvi <olofvi89@gmail.com>
2017-09-24 12:05:14 +0200

    now getting another error message on server

--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -2,6 +2,6 @@ Given(/^I am on the "([^"]*)" page$/) do |page|
   expect(page).to eq "Learning Rails 5"
 end
 
-Then(/^I should see on "([^"]*)" on page$/) do |comment|
-  expect(article.comment).to eq comment
+Then(/^I should see on "([^"]*)" on page$/) do |page|
+  expect(page).to have_field(id, :type => string , :disabled => true)
 end
